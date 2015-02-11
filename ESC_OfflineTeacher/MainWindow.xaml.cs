7c4eabfeb65863ef9d17c2ff58262fe979df7ee0
samthedev <abdelkhalekdev@gmail.com>
2015-02-11 03:46:20 +0100

    Initialisation Commit

---
+++ ESC_OfflineTeacher/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using ESC_OfflineTeacher.ViewModel;
+
+namespace ESC_OfflineTeacher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
